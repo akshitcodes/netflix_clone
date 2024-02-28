00b99723e7afce5d8d52de312ff19d8112e0acc6
akshitcodes <116125986+akshitcodes@users.noreply.github.com>
2024-02-28 18:15:13 +0530

    modified

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const login=document.getElementById("login")
+
+const start=document.getElementById("start")
 login.addEventListener("click",()=>{
+    window.location.href="login.html"
+})
+start.addEventListener("click",()=>{
     window.location.href="login.html"
 })
